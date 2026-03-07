d747084a26d631f64fc4f2db7848d03ada2f7abe
Ruslan Karabalin <karabalin112@gmail.com>
2026-03-07 13:46:10 +0300

    feat: add .gitlab-ci.yml

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log/slog"
+	"net/http"
+	"os"
+	"time"
+)
 
+func main() {
+	mux := http.NewServeMux()
+
+	mux.Handle("GET /", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "WORK?")
+	}))
+
+	srv := &http.Server{
+		Addr:         "localhost:8080",
+		Handler:      mux,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  120 * time.Second,
+	}
+
+	if err := srv.ListenAndServe(); err != nil {
+		slog.Error("Server starting failed", slog.Any("error", err))
+		os.Exit(1)
+	}
 }
