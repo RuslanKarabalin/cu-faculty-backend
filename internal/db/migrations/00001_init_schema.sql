48e2ed80397f2b1116b2dcc47108879bae201862
Ruslan Karabalin <karabalin112@gmail.com>
2026-03-21 13:41:22 +0300

    feat: fix schema and start migration setup

--- internal/db/migrations/00001_init_schema.sql
+++ internal/db/migrations/00001_init_schema.sql
@@ -1,13 +1,54 @@
 -- +goose Up
-create type role_type as enum (
+create type "user_role" as enum (
     'user',
     'admin'
 );
 
-create type social_type as enum (
+create type "social_network" as enum (
     'vk',
     'telegram',
     'github'
+);
+
+create type "edu_grade" as enum (
+    'bachelor',
+    'master',
+    'specialist',
+    'postgraduate'
+);
+
+create table "statuses" (
+    id int primary key generated always as identity,
+    content varchar (127) not null
+);
+
+create table "key_skills" (
+    id int primary key generated always as identity,
+    name varchar (31) not null
+);
+
+create table "soft_skills" (
+    id int primary key generated always as identity,
+    name varchar (31) not null
+);
+
+create table "users" (
+    id uuid primary key default gen_random_uuid(),
+    photo_s3_key varchar(255) not null,
+    first_name varchar(31) not null,
+    last_name varchar(31) not null,
+    bio varchar(255) not null,
+    birth_date date not null,
+    status_id int references statuses(id),
+    role user_role not null
+);
+
+create table "socials" (
+    id int primary key default gen_random_uuid(),
+    user_id uuid references users(id),
+    social social_network not null,
+    link varchar(127) not null,
+    is_preferred boolean not null
 );
 
 -- +goose Down
