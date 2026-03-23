0ecc69cf823df1b9edf0ad4243b390b66e48e4f7
Ruslan Karabalin <karabalin112@gmail.com>
2026-03-23 20:47:58 +0300

    feat: add migration

--- internal/db/migrations/00001_init_schema.sql
+++ internal/db/migrations/00001_init_schema.sql
@@ -74,7 +74,91 @@ create table "edu_places" (
     , is_studying_now boolean not null
 );
 
+create table "faqs" (
+    id int primary key generated always as identity
+    , question varchar(255) not null
+    , answer varchar(255) not null
+);
+
+create table "announcements" (
+    id uuid primary key default gen_random_uuid()
+    , author_id uuid references users(id)
+    , title varchar(31) not null
+    , content varchar(255) not null
+    , created_at timestamptz not null
+    , is_archived boolean not null
+);
+
+create table "news" (
+    id uuid primary key default gen_random_uuid()
+    , author_id uuid references users(id)
+    , photo_s3_key varchar(255) not null
+    , title varchar(31) not null
+    , content varchar(255) not null
+    , created_at timestamptz not null
+);
+
+create table "events" (
+    id uuid primary key default gen_random_uuid()
+    , author_id uuid references users(id)
+    , photo_s3_key varchar(255) not null
+    , title varchar(31) not null
+    , content varchar(255) not null
+    , place varchar(63) not null
+    , starts_at timestamptz not null
+    , registration_link varchar(255)
+    , created_at timestamptz not null
+);
+
+create table "user_key_skills" (
+    user_id uuid references users(id)
+    , key_skill_id int references key_skills(id)
+    , primary key (user_id, key_skill_id)
+);
+
+create table "user_soft_skills" (
+    user_id uuid references users(id)
+    , soft_skill_id int references soft_skills(id)
+    , primary key (user_id, soft_skill_id)
+);
+
+create table "contacts" (
+    user_id uuid references users(id)
+    , contact_id uuid references users(id)
+    , note varchar(255)
+    , primary key (user_id, contact_id)
+);
+
+create table "saved_users" (
+    user_id uuid references users(id)
+    , saved_user_id uuid references users(id)
+    , primary key (user_id, saved_user_id)
+);
+
+create table "announcement_responses" (
+    user_id uuid references users(id)
+    , announcement_id uuid references announcements(id)
+    , primary key (user_id, announcement_id)
+);
+
+create table "event_responses" (
+    user_id uuid references users(id)
+    , event_id uuid references events(id)
+    , primary key (user_id, event_id)
+);
+
 -- +goose Down
+drop table if exists "event_responses";
+drop table if exists "announcement_responses";
+drop table if exists "saved_users";
+drop table if exists "contacts";
+drop table if exists "user_soft_skills";
+drop table if exists "user_key_skills";
+drop table if exists "events";
+drop table if exists "news";
+drop table if exists "announcements";
+drop table if exists "faqs";
+drop table if exists "edu_places";
 drop table if exists "work_places";
 drop table if exists "socials";
 drop table if exists "users";
