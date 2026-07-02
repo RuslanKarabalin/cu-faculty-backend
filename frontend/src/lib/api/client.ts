893edce710fd374a19ca7c6845187fce316f4ab1
Ruslan Karabalin <karabalin112@gmail.com>
2026-07-02 10:51:18 +0300

    feat: remove admin frontend

--- frontend/src/lib/api/client.ts
+++
@@ -1,7 +0,0 @@
-import { ApiError, buildEndpoints, makeRequest, type Request } from './endpoints';
-
-const request: Request = (path, init = {}) =>
-	makeRequest(`/api${path}`, { credentials: 'same-origin', ...init }, fetch);
-
-export const api = buildEndpoints(request);
-export { ApiError };
