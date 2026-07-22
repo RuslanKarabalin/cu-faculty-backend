558a40e8edd53c4353f87f922a36f3f2526a2f47
Ruslan Karabalin <karabalin112@gmail.com>
2026-07-22 07:34:15 +0300

    feat: update statuses

--- backend/internal/db/migrations/00002_add_statuses.sql
+++ backend/internal/db/migrations/00002_add_statuses.sql
@@ -1,9 +1,10 @@
 -- +goose Up
+insert into "statuses" (content) values ('Ищу проект');
 insert into "statuses" (content) values ('Собираю команду');
 insert into "statuses" (content) values ('Хочу нетворкинг');
-insert into "statuses" (content) values ('Ищу стартап');
 insert into "statuses" (content) values ('Работаю');
-insert into "statuses" (content) values ('Устал');
+insert into "statuses" (content) values ('Хочу на отдых');
+insert into "statuses" (content) values ('Не беспокоить');
 
 -- +goose Down
 truncate table "statuses";
